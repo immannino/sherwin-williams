0ceab075443873de1092df2c5f0aa7e4cb9bf24d
Tones <tonymannino@me.com>
2021-01-09 20:01:54 -0800

    trying this again

--- generate-svg.js
+++ generate-svg.js
@@ -6,17 +6,22 @@ let svgParts = [];
 for (let i in colors) {
     let { name, hex } = colors[i];
 
+    if (name.includes('&')) {
+        name = name.replace('&', '&amp;');
+    }
     const fontColor = determineFontColor(hex);
     svgParts.push(`<div style="background-color: ${hex}; color: ${fontColor};" xmlns="http://www.w3.org/1999/xhtml">${name}</div>`);
 }
 
 const svg = `<?xml version="1.0" standalone="no"?>
-    <svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <svg width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg">
         <style>
             .container {
                 display: flex;
                 flex-direction: row;
                 flex-wrap: wrap;
+                height: 100%;
+                width: 100%;
             }
 
             .container > div {
